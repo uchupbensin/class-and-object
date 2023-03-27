dd40c4ce54ff9ab522679d3dfcf1b761a70c60b0
uchupbensin <aldiyogi.pramono@students.amikom.ac.id>
2023-03-27 17:39:28 +0700

    first commit

---
+++ yogi/yogi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classobj
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Mobil mobil1 = new Mobil();
+
+            mobil1.Warna = "Biru";
+            mobil1.JumlahPintu = 2;
+            mobil1.Merk = "Porsche";
+            mobil1.Model = "911 GT2 RS";
+            mobil1.TahunKeluar = 2023;
+
+            mobil1.Gas(293);
+            mobil1.Klakson("Tiin");
+            mobil1.Tampilkaninfo();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
---
+++ yogi/yogi/mobil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Mobil
+{
+    //propertis
+    public string Warna { get; set; }
+    public int JumlahPintu { get; set; }
+    public string Merk { get; set; }
+    public string Model { get; set; }
+    public int TahunKeluar { get; set; }
+
+    //method
+    public void Gas(int kecepatan)
+    {
+        Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", Model, kecepatan);
+    }
+    public void Klakson(string suara)
+    {
+        Console.WriteLine("Bunyi klakson{0}", suara);
+    }
+
+    public void Tampilkaninfo()
+    {
+        Console.WriteLine("Mobil saya berwarna {0} merk {1} model {2} keluaran tahun {3} dengan jumlah pintu {4}", Warna, Merk, Model, TahunKeluar, JumlahPintu);
+
+    }
+
+}
